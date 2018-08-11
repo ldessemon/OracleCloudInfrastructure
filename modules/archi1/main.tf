a195cad887f1e3fb2e40cbe8bb4469dc1a43151c
ldessemon <ludovic.dessemon@gmail.com>
2018-08-11 19:27:30 -0400

    v1.4

--- modules/archi1/main.tf
+++ modules/archi1/main.tf
@@ -196,7 +196,7 @@ resource "oci_core_subnet" "vcn1_public_subnet" {
 	#Required
     display_name = "Public Subnet"
 	availability_domain = "${var.public_subnet_availability_domain}"
-	cidr_block = "${var.public_subnet_cidr_block}"
+	destination = "${var.public_subnet_cidr_block}"
 	compartment_id = "${var.compartment_ocid}"
 	security_list_ids = ["${oci_core_security_list.vcn1_public_seclist.id}"]
 	vcn_id = "${oci_core_vcn.vcn1.id}"
@@ -207,7 +207,7 @@ resource "oci_core_subnet" "vcn1_private_subnet" {
 	#Required
     display_name = "Private Subnet"
 	availability_domain = "${var.private_subnet_availability_domain}"
-	cidr_block = "${var.private_subnet_cidr_block}"
+	destination = "${var.private_subnet_cidr_block}"
 	compartment_id = "${var.compartment_ocid}"
 	security_list_ids = ["${oci_core_security_list.vcn1_private_seclist.id}"]
 	vcn_id = "${oci_core_vcn.vcn1.id}"
@@ -217,7 +217,7 @@ resource "oci_core_subnet" "vcn1_dmz_subnet" {
 	#Required
     display_name = "DMZ Subnet"
 	availability_domain = "${var.dmz_subnet_availability_domain}"
-	cidr_block = "${var.dmz_subnet_cidr_block}"
+	destination = "${var.dmz_subnet_cidr_block}"
 	compartment_id = "${var.compartment_ocid}"
 	security_list_ids = ["${oci_core_security_list.vcn1_dmz_seclist.id}"]
 	vcn_id = "${oci_core_vcn.vcn1.id}"
@@ -230,7 +230,7 @@ resource "oci_core_instance" "NatInstance" {
     availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
     compartment_id = "${var.compartment_ocid}"
     display_name = "nat_instance"
-    image = "${var.InstanceImageOCID[var.region]}"
+    source_details = "${var.InstanceImageOCID[var.region]}"
     shape = "${var.InstanceShape}"
     subnet_id = "${oci_core_subnet.vcn1_public_subnet.id}"
     hostname_label = "nat_instance"
@@ -240,7 +240,7 @@ resource "oci_core_instance" "NatInstance" {
     }
     metadata {
       ssh_authorized_keys = "${var.ssh_public_key}"
-      user_data = "${base64encode(file("user_data.tpl"))}"
+      user_data = "${base64encode(file("./user_data.tpl"))}"
     }
     timeouts {
         create = "10m"
@@ -254,7 +254,7 @@ resource "oci_core_instance" "PrivateInstance" {
     availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
     compartment_id = "${var.compartment_ocid}"
     display_name = "PrivateInstance"
-    image = "${var.InstanceImageOCID[var.region]}"
+    source_details = "${var.InstanceImageOCID[var.region]}"
     shape = "${var.InstanceShape}"
     create_vnic_details {
       subnet_id = "${oci_core_subnet.vcn1_private_subnet.id}"
